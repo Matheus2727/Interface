d431bc3a53a0a173694b0d412691ae2068eb0bde
Matheus2727 <matheus27092000@gmail.com>
2021-11-01 14:09:33 -0300

    comentando

--- src/implementar_interface.py
+++ src/implementar_interface.py
@@ -2,26 +2,31 @@ import interface
 
 
 def exemplo(**kwargs):
+    """função de exemplo par aum botão"""
     print("a")
 
 
 def setarbots(janela: interface.Janela):
+    """seta os botoes e os adiciona a janela"""
     bot1 = interface.Botao(10, 10, 0, 0, "exemplo", "1", 30,
                            [120, 120, 120], exemplo)
     janela.addBotões([bot1])
 
 
 def setartextos(janela: interface.Janela):
+    """seta os textos e os adiciona a janela"""
     text1 = interface.Texto(10, 50, 30, "aaa111aa\naaa", "1")
     janela.addTextos([text1])
 
 
 def setarinputs(janela: interface.Janela):
+    """seta os inputs e os adiciona a janela"""
     inpu1 = interface.Inp(10, 130, 10, 30, "", "1")
     janela.addInputs([inpu1])
 
 
 def setarquads(janela: interface.Janela):
+    "seta os quadrados e os adiciona a janela"
     quad1 = interface.Quadrado(150, 50, 10, [10, 10, 10])
     janela.addQuads([quad1])
 
